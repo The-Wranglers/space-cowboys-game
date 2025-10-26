9171b383c39d2aa5d3f08494d33ae2991a7b9dc2
Daniel Morman <dmorman2006@gmail.com>
2025-10-25 20:54:03 -0500

    Main menu update

--- mainMenu.py
+++ mainMenu.py
@@ -15,13 +15,13 @@ class MainMenu:
         text_rect = text_surface.get_rect(center=(screen.get_width() / 2, 80))
         screen.blit(text_surface, text_rect)
         # Play Text 
-        play_rect = play_text.get_rect(center=(screen.get_width() / 2, 120))
+        play_rect = play_text.get_rect(center=(screen.get_width() / 2, 160))
         screen.blit(play_text, play_rect)
         # Leadership Board Text
-        leadership_rect = leadership_text.get_rect(center=(screen.get_width() / 2, 140))
-        screen.blit(leadership_text, text_rect)
+        leadership_rect = leadership_text.get_rect(center=(screen.get_width() / 2, 240))
+        screen.blit(leadership_text, leadership_rect)
         # Instructions 
-        instruction_rect = instruction_text.get_rect(center=(screen.get_width() / 2, 180))
+        instruction_rect = instruction_text.get_rect(center=(screen.get_width() / 2, 320))
         screen.blit(instruction_text, instruction_rect)
 
     def load_options(self, screen):
